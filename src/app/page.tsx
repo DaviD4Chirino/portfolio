ab6cd22082168302427b15b12da56c0605285d39
David <genarodavid@gmail.com>
2023-07-07 14:40:34 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
 "use client";
-import { Button } from "@mui/material";
+import "./sass/Home.sass";
 
+import { PiMoonFill as MoonI } from "react-icons/pi";
 export default function Home() {
   return (
-    <main>
-      <h1>
-        Hello <Button>Hi</Button>
-      </h1>
+    <main id="Home">
+      <Header />
     </main>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <div className="container">
+        <div id="Banner">
+          <div className="hero">
+            <div className="wrapper relative">
+              <h1 className="hero-1">David</h1>
+              <h1 className="hero-2">Space</h1>
+              <MoonI className="absolute icon" />
+            </div>
+          </div>
+          <div className="slogan">
+            <b>Where ideas comes to thrive</b>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
